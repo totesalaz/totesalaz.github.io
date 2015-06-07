d4843a9c9a81d4c2841fd75b1ed7e152e67d9696
Luis Jose Salazar-Serrano <totesalaz@gmail.com>
2015-06-07 02:28:16 +0200

    bluidea theme selected

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Select pelican theme
+THEME = '/github/pelican_themes/blueidea'
